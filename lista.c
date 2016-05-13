502d3697f46e7ba92fcd0d7526ab186ff96f8469
sinayra <nayra@hotmail.co.jp>
2016-05-13 00:51:30 -0300

    Otimizando lista.c
    Coisas bobas

--- lista.c
+++ lista.c
@@ -9,11 +9,11 @@ int get_n_simbolos(){
 
 void inserir_elemento_no_final(TS novo_valor) {
     TS *lista_aux;
+    
     n_simbolos++;
-
     lista_aux = (TS*) realloc (lista, n_simbolos * sizeof(TS));
 
-    if (lista_aux != NULL) {
+    if (lista_aux) {
         lista = lista_aux;
         lista[n_simbolos - 1] = novo_valor;
     }
@@ -31,11 +31,11 @@ void editar_elemento(int indice, TS linha){
 }
 
 int existe_elemento(char *valor_procura) {
-
-    if(lista == NULL) return -1;
-    for (int i = 0; i < n_simbolos; i++){
-        if(strcmp(lista[i].cadeia, valor_procura) == 0)
-            return i;
+    if(lista){
+    	for (int i = 0; i < n_simbolos; i++){
+        	if(strcmp(lista[i].cadeia, valor_procura) == 0)
+        	    return i;
+    	}
     }
     return -1;
 }
@@ -47,16 +47,13 @@ TS buscar_elemento_indice(int indice_busca) {
     strcpy(aux.tipo, "");
     aux.usado = -1;
 
-    if(lista == NULL) return aux;
-	if(indice_busca >  n_simbolos || indice_busca < 0) return aux;
-
-    aux = lista[indice_busca];
+    if(lista && (indice_busca < n_simbolos && indice_busca >= 0))
+    	aux = lista[indice_busca];
 
     return aux;
 }
 
 void imprimir_lista() {
-
     if(lista){
         printf("\n******TABELA DE SIMBOLOS*****\n\nNome\tTipo\tUsado\n");		/*Imprime tabela de simbolos*/
     	for (int i = 0; i < n_simbolos; i++)
